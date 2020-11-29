e18d360c671f1861068df675f5cb87d9a661b309
kdun2491 <kdun2491@uni.sydney.edu.au>
2020-11-29 11:23:19 +0000

    nav bar

---
+++ script.js
@@ -0,0 +1,6 @@
+function menuToggle() {
+    document.getElementById("nav-slide").classList.toggle("menu-slide-main-on");
+    document.getElementById("menu-bg").classList.toggle("menu-slide-bg-on");
+    document.getElementById("nav-slide").classList.toggle("menu-slide-main-off");
+    document.getElementById("menu-bg").classList.toggle("menu-slide-bg-off");
+}
